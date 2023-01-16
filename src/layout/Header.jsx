107a3a8fc762128c9ed02f55d554d2cebebe182c
Carlos Salazar <crslarez@gmail.com>
2023-01-15 21:05:11 -0500

    clear app

---
+++ src/layout/Header.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import { Nav } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+       <h1>header</h1>
+      </>
+    );
+  }
+}
+
+export default Header;
