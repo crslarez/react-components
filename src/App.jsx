73d62d5abb76dd5e82aa253a6440ea5cdaa65553
Carlos Salazar <crslarez@gmail.com>
2023-01-15 22:07:55 -0500

    add app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Component } from "react";
 import Creations from "./components/Creations";
 import CreationsDesktop from "./components/CreationsDesktop";
+import Session2 from './components/Session2'
+import Footer from './components/Footer'
+import "./scss/index.css"
+
 
 const App = () => {
   return (
     <>
+      <Session2 />
       <Creations />
       <CreationsDesktop />
+      <Footer />
     </>
   );
 };
