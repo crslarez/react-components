725cc227694989cc44ce970e5ceca9b565e165c6
Carlos Salazar <crslarez@gmail.com>
2023-01-17 17:49:43 -0500

    delete coment

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,29 +1,5 @@
 import { Component, useState } from "react";
 import "./Header.css";
-// class Header extends Component {
-//   render() {
-//     const [isOpen, setIsOpen] = useState(false)
-//     return (
-//       <>
-//         <div className="navbar">
-//           <div className="nav-logo">loopstudio</div>
-//           <div className={`nav-items ${isOpen && "open"}`}>
-//             <a href="#">About</a>
-//             <a href="#">Carrers</a>
-//             <a href="#">Events</a>
-//             <a href="#">Products</a>
-//             <a href="#">Support</a>
-//           </div>
-//           <div className={`nav-toogle ${isOpen && "open"}`} id="burger" onClick={() => setIsOpen(!isOpen)}>
-//             <span></span>
-//             <span></span>
-//             <span></span>
-//           </div>
-//         </div>
-//       </>
-//     );
-//   }
-// }
 
 import React from 'react'
 
